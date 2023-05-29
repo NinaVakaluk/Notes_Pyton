7fc3758087ae566311b8409c66f4d42a62abf5c6
Nina <you@example.com>
2023-05-29 23:15:52 +0300

    Добавление функций чтения и записи заметок

---
+++ operation.py
@@ -0,0 +1,37 @@
+import Note
+
+
+def write_file(array, mode):
+    file = open("file_note.csv", mode='w', encoding='utf-8')
+    file.seek(0)
+    file.close()
+    file = open("file_note.csv", mode=mode, encoding='utf-8')
+    for notes in array:
+        file.write(Note.Note.to_string(notes))
+        file.write('\n')
+    file.close
+
+
+def read_file():
+    try:
+        array = []
+        file = open("file_note.csv", "r", encoding='utf-8')
+        notes = file.read().strip().split("\n")
+        for n in notes:
+            split_n = n.split(';')
+            print(split_n[0])
+            note = Note.Note(
+                id = split_n[0], title = split_n[1], body = split_n[2], date = split_n[3])
+            array.append(note)
+    except Exception:
+        print('Нет заметок!')
+    finally:
+        return array
+
+
+
+
+
+
+
+        
